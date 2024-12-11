e57b59aa4dca7f921a23569e446090c219e9e64e
Jort Brouwers <72700048+JortBr@users.noreply.github.com>
2024-12-11 14:20:19 +0100

    Adding website files

---
+++ Website SmokeGuard V1/script.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#learn-more", {
+  duration: 1,
+  scale: 0.8,
+  opacity: 1,
+  delay: 1,
+});
+
+document.querySelector("#learn-more").addEventListener("click", () => {
+  gsap.to(window, {
+    scrollTo: { y: ".features", offsetY: 50 },
+    duration: 1,
+  });
+});
+
+gsap.to(".parallax", {
+  y: "-20%", // Parallax scroll effect
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    end: "bottom top",
+    scrub: true, // Synchroniseert met scroll
+  },
+});
